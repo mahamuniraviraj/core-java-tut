69f8466973446bb71da02ec1a2a5b0ded7f8269a
Raviraj Mahamuni <mahamuniraviraj@gmail.com>
2017-08-29 01:48:49 +0530

    statement, variable declaration,accessing variable and evaluate expression
    Signed-off-by: Raviraj Mahamuni <mahamuniraviraj@gmail.com>

--- 002-java-primitive-types/src/main/java/Main.java
+++ 002-java-primitive-types/src/main/java/Main.java
@@ -4,6 +4,24 @@ public class Main {
 
         System.out.println("Hello Again !!!");
 
+        //Variable Declaration : datatype and variable name
+        int a;
+
+        //Variable assignment or initialization
+        a = 5;
+
+        //  variable declaration and initialization in one statement
+        int b = 0;
+
+        // accessing the value of the variable  a
+        // expression has two parts operands and operator
+        // two operands are variable (a)& integer literal(10)and operator is +
+        // assigning result of expression to another variable
+        b = a + 10;
+
+
+        // Primitive Datatypes available in Java with their minimum and maximum values
+
         //8 bits 1 Byte
         byte byteMin = -128;
         byte byteMax = 127;
@@ -27,6 +45,15 @@ public class Main {
         //64 bits 8 Bytes
         double doubleMin = 4.9E-324;
         double doubleMax = 1.7976931348623157E308;
+
+        //16 bits Unicode
+        char char1 = 'A';
+
+        //chars can be represented as numbers , 0 to 65536
+        char char2 = 66;
+
+        System.out.println(" Char 1 " + char1);
+        System.out.println(" Char 2 " + char2);
 
 
         System.out.println(" byte Min " + byteMin);
